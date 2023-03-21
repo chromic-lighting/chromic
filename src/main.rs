90a723dee38388382560797f7380dbdf76915cdf
James Goodall <22132998+TheGoodall@users.noreply.github.com>
2023-03-21 21:26:14 +0000

    refactor!: Remove support for library usage. (#5)
    In order to reduce complexity, the original project scope (multiple
    applications relying on one central server over a network) has been
    abandoned in favour of a single mono-application. Thus the crate is now
    configured to compile into a single binary, which will provide both
    video and lighting capabilities. Functionality to connect multiple
    instances together may be added later.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use chromic::graph;
+// Public to prevent unused code warnings
+// TODO: Make private again when no longer needed
+pub mod graph;
 
 fn main() -> anyhow::Result<()> {
     let _ = graph::Graph::new();
