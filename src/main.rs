bd853132e314543c5c29a49717c470b4334460e8
James Goodall <jatgoodall@protonmail.com>
2023-03-22 15:26:50 +0000

    Update readme documentation and copy it to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! # Chromic
+//!
+//! Chromic is a processing engine for video and lighting data.
+//!
+//! It is a graph based program for routing video and lighting data from generators, cues, etc to outputs (Video outputs, DMX outputs)
+//!
+//! It is designed so that data can flow through the graph from sources to outputs, but metadata can flow backwards, allowing earlier nodes to easily adapt to changing outputs.
+
 // Public to prevent unused code warnings
 // TODO: Make private again when no longer needed
 pub mod graph;
