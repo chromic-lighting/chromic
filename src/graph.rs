acb863a5b8a0d6acafc21bead855046299ddd173
James Goodall <jatgoodall@protonmail.com>
2023-05-27 13:31:08 +0100

    Wrote add node and remove node functions

--- src/graph.rs
+++ src/graph.rs
@@ -8,7 +8,10 @@ use petgraph::{
 use async_trait::async_trait;
 use smol_str::SmolStr;
 
-use tokio::sync::{mpsc, oneshot, watch};
+use tokio::{
+    sync::{mpsc, oneshot, watch},
+    task,
+};
 
 pub mod data_types;
 
@@ -28,11 +31,17 @@ pub trait Node: Sync {
     async fn run(&self, cmd_chan: mpsc::Receiver<NodeCtrl>) {}
 }
 
+pub struct NodeContainer {
+    node: Box<dyn Node>,
+    handle: task::JoinHandle<()>,
+    cmd_channel: mpsc::Sender<NodeCtrl>,
+}
+
 /// An edge is a connection between two ports.
 pub struct Edge(pub SmolStr, pub SmolStr);
 
 /// A graph is a collection of nodes and edges.
-pub struct Graph(StableGraph<Box<dyn Node>, Edge, Directed>);
+pub struct Graph(StableGraph<NodeContainer, Edge, Directed>);
 
 impl Graph {
     /// Create a new, empty graph.
@@ -50,22 +59,35 @@ impl Default for Graph {
 impl Graph {
     /// Add a node to the graph.
     pub fn add_node(&mut self, node: Box<dyn Node>) -> NodeIndex {
-        self.0.add_node(node)
+        let (snd, rcv) = mpsc::channel(1);
+        let nc: NodeContainer = NodeContainer {
+            node,
+            handle: task::spawn(node.run(rcv)),
+            cmd_channel: snd,
+        };
+        self.0.add_node(nc)
     }
 
     /// Remove a node from the graph, returning the node, if it exists.
-    pub fn remove_node(&mut self, i: NodeIndex) -> Option<Box<dyn Node>> {
-        self.0.remove_node(i)
+    pub async fn remove_node(&mut self, i: NodeIndex) -> bool {
+        let maybe_nc = self.0.remove_node(i);
+        match maybe_nc {
+            Some(nc) => {
+                nc.cmd_channel.send(NodeCtrl::Shutdown).await;
+                nc.handle.await.is_ok()
+            }
+            None => false,
+        }
     }
 
     /// Get a specified node from the graph without removing it.
-    pub fn get_node(&self, i: NodeIndex) -> Option<&dyn Node> {
-        self.0.node_weight(i).map(|n| n.as_ref())
+    pub fn get_node(&self, i: NodeIndex) -> Option<&NodeContainer> {
+        self.0.node_weight(i)
     }
 
     /// Mutably get a specified node from the graph without removing it.
-    pub fn get_mut_node(&mut self, i: NodeIndex) -> Option<&mut (dyn Node + 'static)> {
-        self.0.node_weight_mut(i).map(|n| n.as_mut())
+    pub fn get_mut_node(&mut self, i: NodeIndex) -> Option<&mut NodeContainer> {
+        self.0.node_weight_mut(i)
     }
 
     /// Add an edge between two ports.
@@ -83,13 +105,8 @@ impl Graph {
         self.0.clear_edges();
     }
 
-    pub fn get_nodes(&self) -> impl Iterator<Item = &dyn Node> {
-        self.0.node_weights().map(|n| n.as_ref())
-    }
-
-    pub fn walk_upwards(&self) -> anyhow::Result<Vec<NodeIndex>> {
-        petgraph::algo::toposort(&self.0, None)
-            .map_err(|_| anyhow::anyhow!("Graph contained a cycle"))
+    pub fn get_nodes(&self) -> impl Iterator<Item = &NodeContainer> {
+        self.0.node_weights()
     }
 }
 
